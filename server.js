67579af385b7f6e1209ab6e151a3669f44c5e1fa
shaznan123 <shaznan@knod.sg>
2022-03-10 21:23:46 +0530

    connect to mongodb, add mongoose, add schema

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 //define the app file only after the content in the config file is read
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+//Get mongoDB connection string, replace password with password variable
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//Connect to the database , the second object is to get rid of the deprecation warnings
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((connection) => console.log('Connection was successful!'))
+  .catch((err) => console.log('Connection to the server failed'));
+
+//1) First define a schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    //required the string is to show error message
+    required: [true, 'Tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: String,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have price'],
+  },
+});
+
+//2) Then define a modal (use first upper case - usual convention)
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT;
 app.listen(port, () => {

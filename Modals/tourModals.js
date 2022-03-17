789416081e40f00f45bab7895a9fda2d7a7d6e97
shaznan123 <shaznanfairozesally@gmail.com>
2022-03-17 15:37:38 +0530

    define new mongoose scheme modal

--- Modals/tourModals.js
+++ Modals/tourModals.js
@@ -9,15 +9,54 @@ const tourSchema = new mongoose.Schema({
     //required the string is to show error message
     required: [true, 'Tour must have a name'],
     unique: true,
+    //trim is a schema type available only for strings, it removes whitespace from begining and end
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: String,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour must have price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  //an array of strings
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 //2) Then define a modal (use first upper case - usual convention)
